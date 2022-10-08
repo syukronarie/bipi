ec86b95ea4b38b3b3c0eab817d2628b3ce3b6c8e
syukronarie <syukronarie@gmail.com>
2022-10-08 10:55:08 +0700

    fix(linter): fixed linter
    updated src/config/config.js
    
     updated src/config/logger.js
    
     updated
    src/config/morgan.js
    
     updated src/middlewars/error.js

--- bipi-svc/src/middlewares/error.js
+++ bipi-svc/src/middlewares/error.js
@@ -7,7 +7,9 @@ const errorConverter = (err, req, res, next) => {
   let error = err;
   if (!(error instanceof ApiError)) {
     const statusCode =
-      error.statusCode || error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+      error.statusCode || error
+        ? httpStatus.BAD_REQUEST
+        : httpStatus.INTERNAL_SERVER_ERROR;
     const message = error.message || httpStatus[statusCode];
     error = new ApiError(statusCode, message, false, err.stack);
   }
