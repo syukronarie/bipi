46e816862fc730828c23556fb18e33029ba7cb4a
syukronarie <syukronarie@gmail.com>
2022-10-08 13:55:12 +0700

    chore(const): updated ErrorMessage.js

--- bipi-svc/src/utils/ErrorMessages.js
+++ bipi-svc/src/utils/ErrorMessages.js
@@ -4,6 +4,7 @@ const ErrorMessage = {
   INTERNAL_SERVER_ERROR: "Internal server error",
   MANDATORY_FIELD_MISSING_ERROR: "Mandatory Field/s missing",
   ID_MISSING: "ID is missing",
+  INPUT_IS_ACTIVE_IS_BOOLEAN: "Input `isActive` is boolean",
 };
 
 export default ErrorMessage;
