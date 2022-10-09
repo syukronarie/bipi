d0289fce25f899c9e1af91b7425b6d179f5d6c20
syukronarie <syukronarie@gmail.com>
2022-10-09 13:50:20 +0700

    docs(release/v1.x): initial release v1.x

--- bipi-svc/dist/models/Constants.js
+++ bipi-svc/dist/models/Constants.js
@@ -6,7 +6,12 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var CONST = {
   FALSE: "FALSE",
-  SUCCESS: "SUCCESS"
+  SUCCESS: "SUCCESS",
+  MERCHANT_SUCCESS_CREATED: "Merchant was successfully created",
+  MERCHANT_SUCCESS_UPDATED: "Merchant was successfully updated",
+  BULK_IS_ACTIVE_SUCCESS: function BULK_IS_ACTIVE_SUCCESS(isActive) {
+    return "ALL merchant's `isActive` were successfully updated to `".concat(isActive, "`");
+  }
 };
 Object.freeze(CONST);
 var _default = CONST;
